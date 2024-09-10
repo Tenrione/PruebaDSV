0fffc9137105c1d8cf1b3ea1d89ec0839548f388
Tenrione <erielten13@gmail.com>
2024-09-10 09:04:21 -0500

    Se arregla el error de repeticion por merge

--- PruebaDSV/Form1.Designer.cs
+++ PruebaDSV/Form1.Designer.cs
@@ -34,9 +34,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(574, 99);
+            this.button1.Location = new System.Drawing.Point(430, 80);
+            this.button1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(153, 60);
+            this.button1.Size = new System.Drawing.Size(115, 49);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -44,20 +45,23 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(163, 118);
+            this.button2.Location = new System.Drawing.Point(122, 96);
+            this.button2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(152, 59);
+            this.button2.Size = new System.Drawing.Size(114, 48);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- PruebaDSV/Form1.cs
+++ PruebaDSV/Form1.cs
@@ -17,19 +17,19 @@ namespace PruebaDSV
             InitializeComponent();
         }
 
-        private void button2_Click(object sender, EventArgs e)
-        {
-            Form3 form3 = new Form3();  
-            this.Hide();
-            form3.Show();
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
             Form2 form2 = new Form2();
             this.Hide();
             form2.Show();
 
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form3 form3 = new Form3();
+            this.Hide();
+            form3.Show();
         }
     }
 }
